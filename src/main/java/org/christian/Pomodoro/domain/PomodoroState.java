ade898e747b94fbb3baf77eaa772f85b7dd95e45
cristianfernandezdev <cristianfernandez.dev@gmail.com>
2026-01-05 19:39:07 +0100

    Update PomodoroState.java

--- src/main/java/org/christian/Pomodoro/domain/PomodoroState.java
+++ src/main/java/org/christian/Pomodoro/domain/PomodoroState.java
@@ -39,14 +39,12 @@ public class PomodoroState {
         isRunning = running;
     }
 
-    // --- NUEVO MÉTODO PARA QUE FUNCIONE LA BARRA CIRCULAR ---
     public double getProgress() {
-        // Calculamos el % completado basándonos en 25 minutos (1500 segundos)
-        // Si quieres que sea dinámico, habría que guardar el "tiempo total" en una variable.
+       
         double totalSeconds = 25 * 60.0;
         double currentSeconds = (minutes * 60) + seconds;
 
-        // Devuelve un valor entre 0.0 y 1.0
+      
         return currentSeconds / totalSeconds;
     }
 }
